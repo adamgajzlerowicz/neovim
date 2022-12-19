b9d8ef8672c609c0447675bf2984fa755843d0b4
Adam Gajzlerowicz <nelf86@gmail.com>
2022-12-19 19:41:56 +0100

    push

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -5,7 +5,7 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<tab><tab>', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set("n", "<leader>t", builtin.treesitter, {})
 
 
 --vim.keymap.set('n', '<leader>ps', function()
