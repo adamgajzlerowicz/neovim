1c89045b5cc025413fcfda50169ed3cf133801fd
Adam Gajzlerowicz <nelf86@gmail.com>
2022-12-19 20:36:58 +0100

    asdf

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,14 +1,10 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>p', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<tab><tab>', builtin.buffers, {})
-vim.keymap.set("n", "<leader>t", builtin.treesitter, {})
+vim.keymap.set('n', '<leader>ff', builtin.live_grep, {})
+vim.keymap.set('n', '<tab><tab>', builtin.git_files, {})
 
-
---vim.keymap.set('n', '<leader>ps', function()
---	builtin.grep_string({ search = vim.fn.input("Grep > ") })
---end)
+vim.keymap.set("n", "<leader>r", builtin.lsp_document_symbols, {})
+vim.keymap.set("n", "<leader>t", builtin.lsp_workspace_symbols, {})
+vim.keymap.set("n", "<leader>u", builtin.lsp_references, {})
 
